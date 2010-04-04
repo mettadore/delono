19117aaf1467c878198faecb9d668b7f71186577
mettadore <john@mettadore.com>
2010-04-04 13:00:12 -0700

    Add log creation to deployment.

--- config/deploy.rb
+++ config/deploy.rb
@@ -34,7 +34,8 @@ namespace :deploy do
   desc "Link in the production database.yml" 
   task :after_update_code do
     run "ln -nfs #{deploy_to}/#{shared_dir}/config/database.yml #{release_path}/config/database.yml" 
-    run "mkdir #{release_path}/log && touch #{release_path}/log/production.log"
+    run "mkdir #{release_path}/log"
+    run "touch #{release_path}/log/production.log"
   end
 
 end
