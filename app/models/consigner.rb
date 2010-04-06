e83e6e6afbcb0a9b7a48f36dc44aaa39167e292f
mettadore <john@mettadore.com>
2010-04-06 11:43:21 -0700

    Fixes to consigners model

--- app/models/consigner.rb
+++ app/models/consigner.rb
@@ -7,20 +7,26 @@ class Consigner < ActiveRecord::Base
   
   validates_presence_of :name
   validates_uniqueness_of :name, :biz_name
-  before_validation do
-    self.name.downcase!
-    self.biz_name.downcase if attribute_present?("biz_name")
-  end
-
-  after_validation do
-    self.name.humanize!
-    self.biz_name.humanize! if attribute_present?("biz_name")
-  end
-  
+  before_validation :downcase_name
+  after_validation :humanize_name 
+    
   def invoice!(invoice)
     self.last_invoice = invoice
     self.save!
   end
   def bizname; biz_name; end
   def name_or_bizname; biz_name ? biz_name : name; end
+
+  private
+  
+  def downcase_name
+    self.name.downcase!
+    self.biz_name.downcase if attribute_present?("biz_name")
+  end
+  
+  def humanize_name
+    self.name = self.name.humanize
+    self.biz_name = self.biz_name.humanize if attribute_present?("biz_name")
+  end
+
 end
