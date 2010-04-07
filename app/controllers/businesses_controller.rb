ef0a5cc7320fc08712f0b4fbf9d69cf66bba1eb8
mettadore <john@mettadore.com>
2010-04-06 17:29:43 -0700

    Add business name in header

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -1,5 +1,5 @@
 class BusinessesController < ApplicationController
-  before_filter :login_required
+  before_filter :login_required, :get_business_by_subdomain
   # GET /businesses
   # GET /businesses.xml
   def index
